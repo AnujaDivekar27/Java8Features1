3a3b271f45d449528f0ed85ca8c41be802cc1ec0
anuja274 <divekar.anuja27@gmail.com>
2023-11-25 09:48:42 +0530

    static method addes

--- NewJavaProject/DefaultMethodEx/src/com/demo/Class1.java
+++ NewJavaProject/DefaultMethodEx/src/com/demo/Class1.java
@@ -13,9 +13,13 @@ public class Class1 implements I1,I2
 
 	public static void main(String[] args) {
 		
+		I1.clean(); //static method of I1
+		
 		I1 i1=new Class1();
+		I1.clean();
 		i1.m1();  //abstract method
 		i1.m3();  //default method
+		
 		
 		I2 i2=new Class1();
 		i2.m2();  //default method
--- NewJavaProject/DefaultMethodEx/src/com/demo/I1.java
+++ NewJavaProject/DefaultMethodEx/src/com/demo/I1.java
@@ -7,4 +7,9 @@ public interface I1 {
 	 {
 		 System.out.println("default m3 method of I1");
 	 }
+	
+	public static void clean()
+	{
+		 System.out.println("static method of I1");
+	}
 }
