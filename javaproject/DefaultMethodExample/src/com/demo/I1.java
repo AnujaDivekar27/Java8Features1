42abc8e87bb0b22d20b6ce15185d1faab516478e
anuja274 <divekar.anuja27@gmail.com>
2023-11-25 09:08:54 +0530

    SimpleProg added

--- javaproject/DefaultMethodExample/src/com/demo/I1.java
+++
@@ -1,6 +0,0 @@
-package com.demo;
-
-public interface I1 {
-	void m1();
-
-}
--- javaproject/GitExamples/src/SimpleExample.java
+++
@@ -1,9 +0,0 @@
-
-public class SimpleExample {
-	
-	public static void name() {
-				
-	  System.out.println("Hi Exponent");
-	  System.out.println("How Are You???");
-		}
-	}	
