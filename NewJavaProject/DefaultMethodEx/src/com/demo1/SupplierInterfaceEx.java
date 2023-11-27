b03d7e718b96178d6e9a5089bc6c550a014e0235
anuja274 <divekar.anuja27@gmail.com>
2023-11-27 08:41:11 +0530

    new files added

---
+++ NewJavaProject/DefaultMethodEx/src/com/demo1/SupplierInterfaceEx.java
@@ -0,0 +1,36 @@
+package com.demo1;
+
+import java.util.function.Supplier;
+
+/*-----------Supplier Interface->java.util.function-----------------
+ * no input only supply,
+ * contains single abstract method: get<t> which returns object
+ ********************************************************************/
+
+public class SupplierInterfaceEx {
+
+	public static void main(String[] args) {
+		
+	  Supplier<String> str=()->{
+		  
+	    String s="Hello";
+	    return s;
+	   
+	  };
+	
+	  System.out.println(str.get());
+	  System.out.println("------------------------------------------");
+	  
+	  Supplier<Student> stud=()->{
+		  
+		 Student s=new Student();
+		 s.setSid(1);s.setSname("Devansh");
+		 return s;
+		   
+	  };
+	
+	  System.out.println(stud.get());
+	}
+	
+	
+}
