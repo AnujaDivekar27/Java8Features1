5cd6e87da8af3302b3d139324122fab88558ad7f
anuja274 <divekar.anuja27@gmail.com>
2023-12-03 08:14:05 +0530

    new files added

--- NewJavaProject/Java8Stream/src/com/example/functions/SortedFunction.java
+++ NewJavaProject/Java8Stream/src/com/example/functions/SortedFunction.java
@@ -1,6 +1,7 @@
 package com.example.functions;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,15 +10,24 @@ public class SortedFunction {
  public static void main(String[] args) {
  
    List<Integer> l1=Arrays.asList(5,3,10,2,8);
-   System.out.println("sort list using sorted() function");
+   System.out.println("sort list in ascending order using sorted() function");
    //sorted():intermediate function sort stream in ascending order
-   System.out.println("===========================================================");
+   System.out.println("===================================================================================");
    
    //l1.stream().sorted().forEach(System.out::println);
    List<Integer> sortedl1=l1.stream().sorted().collect(Collectors.toList());
    System.out.println(sortedl1);  //[2, 3, 5, 8, 10]
  
-   System.out.println("==========================================================");
+   System.out.println("===================================================================================");
+   
+   List<Integer> sortedlreversel1=l1.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+   //reverse sorted stream
+   System.out.println("sort list in reverse order using sorted((Comparator.reverseOrder())");
+   System.out.println( sortedlreversel1); //[10, 8, 5, 3, 2]
+ 
+   System.out.println("===================================================================================");
+ 
+ 
  
  }
 
